8092ca15c4d0e92f9a44e716001f07fa0be1fc78
Reuben Potter <reubenpotter2005@gmail.com>
2026-06-18 16:16:53 +0000

    Uploaded project
    Just have the GUI and clicking function working with a label to display number of clicks.

---
+++ AutoClickerGUI/src/GUI.java
@@ -0,0 +1,44 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUI implements ActionListener {
+
+    int count = 0;
+
+    private JLabel label;
+    private JFrame frame;
+    private JPanel panel;
+
+    public GUI() {
+        frame = new JFrame();
+
+
+        JButton button = new JButton("Click");
+        button.addActionListener(this);
+        label = new JLabel("Number of clicks: 0");
+
+        panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(120, 120, 120, 120));
+        panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+        panel.add(label);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Our AutoClicker.GUI");
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    static void main(String[] args) {
+        new GUI();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        label.setText("Number of clicks: " + count);
+    }
+}
